ce1f20946ed73ae257fe8d9f98f552e188e67c80
juanmq26 <61187772+juanmq26@users.noreply.github.com>
2026-08-27 01:21:10 +0200

    Create projects.js

---
+++ js/js/projects.js
@@ -0,0 +1,22 @@
+const categories = [
+  "all",
+  "people",
+  "animals",
+  "objects",
+  "vehicles",
+  "food",
+  "plants",
+  "architecture"
+];
+
+
+const projects = [
+
+  {
+    category: "objects",
+    image: "BOLSA_1.png",
+    description: "Descripción de la bolsa.",
+    dwg: ""
+  }
+
+];
